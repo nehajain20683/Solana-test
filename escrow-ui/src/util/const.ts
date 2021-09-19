de30ed10c4971dfacdd3d8eaaf09107c44ee14aa
Suraj Singla <surajsingla333@gmail.com>
2021-09-19 23:21:48 +0530

    add contracts file

---
+++ escrow-ui/src/util/const.ts
@@ -0,0 +1,44 @@
+export const accPrivKeyMnemonic = "shoe begin egg stomach damage spoil fade student valid cloud acoustic blouse gentle two tone point cross minimum cry deer mammal common hidden cliff"
+
+export const accPrivKeyByteArr = "226,16,89,132,159,184,182,204,35,77,11,54,57,173,225,189,111,4,213,30,67,170,235,129,154,56,179,192,244,218,216,111,106,220,224,224,145,214,203,181,125,79,162,6,26,179,110,148,191,178,156,139,160,120,159,107,139,73,248,167,70,60,132,36"
+
+export const receiverPrivKeyMnemonic = "cash salon shed blast exit glad romance sand defense badge observe purity very buffalo display agree apart stem crane strike ancient hamster quantum helmet" // A1EyMZ4fdBocvKdQpFhTQ4Q5DwTSZhuLMTErxUjy4uEY
+export const receiverPrivKeyByteArr = "243,130,135,8,180,85,211,133,101,35,39,195,121,180,86,165,140,100,152,225,51,114,110,43,248,35,59,98,76,44,41,82,133,200,250,232,58,210,196,244,239,230,93,37,83,148,42,174,253,207,135,96,115,176,208,67,83,100,74,158,150,109,105,217"
+
+export const tokenAUserAcc = "2HG6aEHFpvZLGFNXxZuEJ3KHn4NBXUGkjTBJYxwXGHEs"
+export const tokenBUserAcc = "4UR5coKmtrW2kHn2mw3td463yixpUHLKBQbStTHTiFSA"
+export const tokenCUserAcc = "ChJKtvRXJ83yMgpdgK8KeNnNDCHXeGbWdq4WMATuLxGe"
+export const tokenDUserAcc = "kCHor25YusBLU7XQq2S3j68nvZYiyWag2RTfW3iT7pK"
+export const tokenEUserAcc = "GGtbea8xorBwVGo8ZALDReNTZcqCqgX6RUD6RP8EJJLm"
+export const tokenFUserAcc = "71YVyyvmsKp9KNb5yid15QddAAj8esaw7ZZpCavWLvZn"
+export const tokenGUserAcc = "9ANzMoHZMsDwpBVfeg5kdZEd6SXZe5k6Rf8R8WtdPubr"
+
+export const tokenAPubKeyString = 'Cga3rxzAeevcULSf7dnKwUwyecFcjD2kD1khTDyyfdfA'
+export const tokenBPubKeyString = 'C3p41Ndvc58UskTTs9WkjPvG6NiMPGCbvUavG6vXrVhN'
+export const tokenCPubKeyString = 'G5dvrN8xkzkWfoCWvFwsuFJfQHaPkyutPr5iwMbU4pbZ'
+export const tokenDPubKeyString = 'E5GAwErW127bs1EaYXXLb3zRvHYVVdp4hZvMPsvW9wuU'
+export const tokenEPubKeyString = 'CwvFgzUxUgMnTb9zCYBfCKgDMUeWNM8zvhDfBdFMrCJt'
+export const tokenFPubKeyString = '8uFKvxP2RrFEuLmVjnPBSZdAkXWQbCyYN5q7SJVEuDdg'
+export const tokenGPubKeyString = 'kmjjyQymk46rFgVMxFfojkjxnLwPAbYssK2MpuyRZNy'
+
+export const tokenAUserAcc_user2 = "93CzoWhDWmxw15FRHhwAHjTGzGpELr7PzzvFds1YGowV"
+export const tokenBUserAcc_user2 = "Gaj8RRAsdhAtLjUYtHMWjjKwvUrXoi6hjdNE8GxuFkK"
+export const tokenCUserAcc_user2 = "6K6NoV9hBRqsZ5RENmK2FNeJZXDfQ9r5EaPQ3vQUGk5i"
+export const tokenDUserAcc_user2 = "BFGnuYZN8oAUQH1maEyBsFvXrnsTEJY1EWBck2QnF5jq"
+export const tokenEUserAcc_user2 = "DM6s2jisPmZvAVDznxvRw7qynLPcrv48ShqQHjTV6Fjc"
+
+export const tokenAmount: number = 1000000000000000
+
+
+/**LATEST */
+
+/**
+ * CiJkL1zKPSfaA9nahzqQk8VYtbyp5WdYxDQhQfMoosnR - ProgamId
+ * 6zcSB1syqfHvaCvg1NA8Dpycsh2dCaWjHCzCkVTPFhZq - Program Account
+ * 9UwH2UtLvasyHPpDgh5TJWSbZ8QGyAjbwGFbME9miNw7 - PDA Token A
+ * EEexpwitPM3rbRS1VUDfvarX4YRjaWM6CmGh5x21sXTE - PDA Token B
+ * 6mBoC6f89T2pY6CuXxPDpJQyH1FZHPwxtiBPLcykbn6A - PDA Token C
+ * 4vVRMUvLt4EvfJAvFgNs6wvZwfLaL9J6R87mh65xaeaf - PDA Token D
+ * 9eiCUEhdX9sy7QstHsmqBJKS5P7hGuXLg9LGUn3WTsmq - PDA Token E
+ * 6XE88SSXBHw8kRKdBcPYLiji79YEW5X5ZJkhzjesReXR - PDA address
+ */
